6426094abfc8e6244931c7fcdf31419e5fdc8327
mohamad rifan baihaqie <rifanbaihaqie@gmail.com>
2018-12-13 07:18:25 +0700

    13122018:0718

---
+++ vendor/jeroennoten/laravel-adminlte/resources/views/page.blade.php
@@ -0,0 +1,155 @@
+@extends('adminlte::master')
+
+@section('adminlte_css')
+    <link rel="stylesheet"
+          href="{{ asset('vendor/adminlte/dist/css/skins/skin-' . config('adminlte.skin', 'blue') . '.min.css')}} ">
+    <link rel="stylesheet"
+          href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <link rel="stylesheet"
+          href="//cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css">
+    <link rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.25.6/sweetalert2.min.css">
+    <link rel="stylesheet"
+          href="{{ asset('css/custom.css') }}">
+    @stack('css')
+    @yield('css')
+@stop
+
+@section('body_class', 'skin-' . config('adminlte.skin', 'blue') . ' sidebar-mini ' . (config('adminlte.layout') ? [
+    'boxed' => 'layout-boxed',
+    'fixed' => 'fixed',
+    'top-nav' => 'layout-top-nav'
+][config('adminlte.layout')] : '') . (config('adminlte.collapse_sidebar') ? ' sidebar-collapse ' : ''))
+
+@section('body')
+    <div class="wrapper">
+
+        <!-- Main Header -->
+        <header class="main-header">
+            @if(config('adminlte.layout') == 'top-nav')
+            <nav class="navbar navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}" class="navbar-brand">
+                            {!! config('adminlte.logo', '<b>Admin</b>LTE') !!}
+                        </a>
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            @each('adminlte::partials.menu-item-top-nav', $adminlte->menu(), 'item')
+                        </ul>
+                    </div>
+                    <!-- /.navbar-collapse -->
+            @else
+            <!-- Logo -->
+            <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}" class="logo">
+                <!-- mini logo for sidebar mini 50x50 pixels -->
+                <span class="logo-mini">{!! config('adminlte.logo_mini', '<b>A</b>LT') !!}</span>
+                <!-- logo for regular state and mobile devices -->
+                <span class="logo-lg">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</span>
+            </a>
+
+            <!-- Header Navbar -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <!-- Sidebar toggle button-->
+                <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                    <span class="sr-only">{{ trans('adminlte::adminlte.toggle_navigation') }}</span>
+                </a>
+            @endif
+                <!-- Navbar Right Menu -->
+                <div class="navbar-custom-menu">
+
+                    <ul class="nav navbar-nav">
+                        <li>
+                            @if(config('adminlte.logout_method') == 'GET' || !config('adminlte.logout_method') && version_compare(\Illuminate\Foundation\Application::VERSION, '5.3.0', '<'))
+                                <a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}">
+                                    <i class="fa fa-fw fa-power-off"></i> {{ trans('adminlte::adminlte.log_out') }}
+                                </a>
+                            @else
+                                <a href="#"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                                >
+                                    <i class="fa fa-fw fa-power-off"></i> {{ trans('adminlte::adminlte.log_out') }}
+                                </a>
+                                <form id="logout-form" action="{{ url(config('adminlte.logout_url', 'auth/logout')) }}" method="POST" style="display: none;">
+                                    @if(config('adminlte.logout_method'))
+                                        {{ method_field(config('adminlte.logout_method')) }}
+                                    @endif
+                                    {{ csrf_field() }}
+                                </form>
+                            @endif
+                        </li>
+                    </ul>
+                </div>
+                @if(config('adminlte.layout') == 'top-nav')
+                </div>
+                @endif
+            </nav>
+        </header>
+
+        @if(config('adminlte.layout') != 'top-nav')
+        <!-- Left side column. contains the logo and sidebar -->
+        <aside class="main-sidebar">
+
+            <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">
+
+                <!-- Sidebar Menu -->
+                <ul class="sidebar-menu" data-widget="tree">
+                    @each('adminlte::partials.menu-item', $adminlte->menu(), 'item')
+                </ul>
+                <!-- /.sidebar-menu -->
+            </section>
+            <!-- /.sidebar -->
+        </aside>
+        @endif
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            @if(config('adminlte.layout') == 'top-nav')
+            <div class="container">
+            @endif
+
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                @yield('content_header')
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+
+                @yield('content')
+
+            </section>
+            <!-- /.content -->
+            @if(config('adminlte.layout') == 'top-nav')
+            </div>
+            <!-- /.container -->
+            @endif
+        </div>
+        <!-- /.content-wrapper -->
+
+    </div>
+    <!-- ./wrapper -->
+@stop
+
+@section('adminlte_js')
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/validator/10.4.0/validator.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.25.6/sweetalert2.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" ></script>
+    <script src="//cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js" ></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" ></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js" ></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js" ></script>
+    <script src="//cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js" ></script>
+    <script src="//cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js" ></script>
+    @stack('js')
+    @yield('js')
+@stop
