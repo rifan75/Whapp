03a9927c0a004e2070aa7c8411f1bf37e503dca8
mohamad rifan baihaqie <rifanbaihaqie@gmail.com>
2018-12-15 08:40:16 +0700

    15122018:0840

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,4 @@
 # Set ownership for all folders
 sudo rm -rf /var/www/html/warehouse-demo/storage/logs/*
 sudo rm -rf /var/www/html/warehouse-demo/storage/framework/views/*
+sudo rm -rf /var/www/html/warehouse-demo/bootstrap/cache/*
