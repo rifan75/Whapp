afaaeda99face3377ebe39a7b26ff9fd7d81c3e6
mohamad rifan baihaqie <rifanbaihaqie@gmail.com>
2019-01-01 06:51:01 +0700

    01012019:0650

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,3 +4,4 @@ sudo rm -rf /var/www/html/warehouse-demo/storage/logs/*
 sudo rm -rf /var/www/html/warehouse-demo/storage/framework/views/*
 sudo rm -rf /var/www/html/warehouse-demo/bootstrap/cache/*
 sudo rm -rf /var/www/html/warehouse-demo/storage/framework/sessions/*
+sudo rm -rf /var/www/html/warehouse-demo/storage/framework/cache/data/*
